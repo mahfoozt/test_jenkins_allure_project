ffec86614777572b6b1d38553050dcb9baaa58a4
mahfoozt <mahfooz.murshalin@gmail.com>
2024-05-29 10:15:39 -0700

    secomt commit after update

--- src/test/java/test_jenkins_allure/Test_Jenkins1.java
+++ src/test/java/test_jenkins_allure/Test_Jenkins1.java
@@ -6,7 +6,7 @@ public class Test_Jenkins1 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Jenkins-----------");
-		
+		System.out.println("Hello Jenkins 22222222222222");
 	}
 
 }
